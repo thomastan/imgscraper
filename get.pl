a253721b8a2af568830ff5c5e339a82bd9714341
thomastan <thomas.tan@msn.com>
2016-03-07 12:32:42 -0700

    Merged getlinks.pl and getimgs.pl into get.pl

--- get.pl
+++ get.pl
@@ -16,13 +16,16 @@ my $req = HTTP::Request->new(GET => $baseurl);
 my $parser = HTML::LinkExtor->new(\&pushlinks, $baseurl);
 
 my @links = ();
+my $tag_name = shift;
+my $attr_name = $tag_name eq "a" ? "href" : $tag_name eq "img" ? "src" : undef;
+
 sub pushlinks {
 	my($tag, %attr) = @_;
-	return if $tag ne 'a';
-	push(@links, $attr{href});
+	return if $tag ne $tag_name;
+	push(@links, $attr{$attr_name});
 }
 
-my $content = $ua->request($req)->decoded_content;  # decoded from UTF-8
+my $content = $ua->request($req)->decoded_content;  # decode from UTF-8
 $parser->parse($content);
 
 print join("\n", @links), "\n";
