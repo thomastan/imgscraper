1be19c392530cf3c2986fb2d46bc664751a9ca81
thomastan <thomas.tan@msn.com>
2016-03-07 10:27:04 -0700

    Add imgscrape.sh, update README.md with requirements

---
+++ imgscrape.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+WEBSITE=$1
+RECURSIONDEPTH=4
+
+declare -A imgs    # array of url's of images found (no duplicates)
+declare -A visited # array of url's of pages visited (so no multi page visits)
+
+# recurse function with depth and URL as $1 and $2 parameters respectively
+function visitpage() {
+	local pgdepth=$1
+	local url=$2
+
+	if [ $pgdepth -eq $RECURSIONDEPTH ]; then
+		return
+	fi
+
+	# save each img url only once by using an associative array
+	for imgsrc in $(perl getimgs.pl $url); do
+		imgs[$imgsrc]=$url  # the page containing the img is saved as the value
+	done
+
+	# get links on page using Perl script
+	for website in $(perl getlinks.pl $url); do
+		[[ "${visited[$website]}" && : ]] || visitpage $(expr $1 + 1) $website
+		visited[$website]=$website
+	done
+}
+
+# start crawling
+visitpage 0 $WEBSITE
+
+if [ ${#imgs[@]} -gt 0 ]; then
+	echo "<html>"
+	echo "<body>"
+
+	for i in "${!imgs[@]}"; do
+		echo '<img src="'$i'" width="25%" />'
+	done
+
+	echo "</body>"
+	echo "</html>"
+fi
